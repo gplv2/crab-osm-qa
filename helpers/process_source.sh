93fd5ac00e1825e505a30c2db8a6617f210d5a54
Glenn Plas <glenn@byte-consult.be>
2017-10-28 23:39:49 +0200

    set encoding to latin1 for the road database

--- helpers/process_source.sh
+++ helpers/process_source.sh
@@ -52,8 +52,8 @@ do
  echo "OGR2OSM"
  echo "======="
  rm -f "${filename}.osm"
- echo /usr/local/bin/ogr2osm/ogr2osm.py --idfile=ogr2osm.id --positive-id --saveid=ogr2osm.id "${filename}_parsed/${filename}.shp"
- /usr/local/bin/ogr2osm/ogr2osm.py --idfile=ogr2osm.id --positive-id --saveid=ogr2osm.id "${filename}_parsed/${filename}.shp"
+ echo /usr/local/bin/ogr2osm/ogr2osm.py --encoding=latin1 --idfile=ogr2osm.id --positive-id --saveid=ogr2osm.id "${filename}_parsed/${filename}.shp"
+ /usr/local/bin/ogr2osm/ogr2osm.py --encoding=latin1 --idfile=ogr2osm.id --positive-id --saveid=ogr2osm.id "${filename}_parsed/${filename}.shp"
  echo ""
 
 done
